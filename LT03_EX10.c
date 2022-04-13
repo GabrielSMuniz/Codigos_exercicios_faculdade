73fb8e0cd3871ab0a2b8b542eb9e06ab4b379a8b
Your Name <you@example.com>
2022-04-12 23:44:36 -0300

    first commit

---
+++ LT03_EX10.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+
+// Programação que recebe 3 valores diferentes e retorna eles em ordem crescente no console.
+
+int main()
+{
+    int a, b, c;
+    while (1)
+    {
+        printf("Primeiro numero: ");
+        scanf("%d", &a);
+        printf("Segundo numero: ");
+        scanf("%d", &b);
+        printf("Terceiro numero: ");
+        scanf("%d", &c);
+        printf("Numeros em ordem crescente: ");
+
+        if (a < b)
+        {
+            if (a < c)
+            {
+                printf("%d - ", a);
+                if (b < c)
+                {
+                    printf("%d - %d", b, c);
+                }
+                else
+                {
+                    printf("%d - %d", c, b);
+                }
+            }
+            else
+            {
+                printf("%d - %d - %d", c, a, b);
+            }
+        }
+        else
+        {
+            if (b < c)
+            {
+                printf("%d - ", b);
+                if (a < c)
+                {
+                    printf("%d - %d", a, c);
+                }
+                else
+                {
+                    printf("%d - %d", c, a);
+                }
+            }
+            else
+            {
+                printf("%d - %d - %d", c, b, a);
+            }
+        }
+        printf("\n\n");
+    }
+    return 0;
+}
