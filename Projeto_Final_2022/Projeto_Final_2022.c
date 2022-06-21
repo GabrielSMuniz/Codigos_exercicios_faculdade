7ef370e9bd18c410fd006b2bdb491b8069ad8f0c
Your Name <you@example.com>
2022-06-21 00:01:39 -0300

    criacao de funçoes novas e criacao do menu

--- Projeto_Final_2022/Projeto_Final_2022.c
+++ Projeto_Final_2022/Projeto_Final_2022.c
@@ -7,11 +7,12 @@ que simule uma carteira de vacinação (COVID).
 
 #include "stdio.h"
 #include "string.h"
+#include "math.h"
 
 #define MAX_PACIENTES 100
 #define MAX_DOSES 5
 
-int qtd_pacientesCadastrados = 0;
+int qtdPacientesCadastrados = 0;
 
 typedef struct Data
 {
@@ -37,22 +38,18 @@ typedef struct CarteiraVacinacao
 
 int validaNome(char nome[])
 {
-    // varifica se a string tem menos de 7 caracteres
-    if (strlen(nome) < 7)
+    // verifica se a string tem menos de 7 caracteres
+    int tam = strlen(nome);
+    if (tam < 7)
         return 0;
     // verifica se string possui 1 espaço e se a distacia dele ao início
     // e ao final é maior que 3
-    for (int i = 0; i < strlen(nome); i++)
+    for (int i = 0; i < tam; i++)
     {
         if (nome[i] == ' ')
         {
-            if (i > 2 && i < strlen(nome) - 3)
-            {
-                for (i++; i < strlen(nome); i++)
-                    if (nome[i] == ' ')
-                        return 0;
+            if (i > 2 && i < tam - 3)
                 return 1;
-            }
         }
     }
     return 0;
@@ -85,14 +82,14 @@ int validaData(Data data)
 
 void cadastraPaciente(CarteiraVacinacao *dPaciente)
 {
-    int qtd_doses;
+    int qtdDosesPaciente;
 
-    printf("--- Credenciais do paciente ---\n");
+    printf("\n--- Credenciais do paciente ---\n");
     printf("Nome: ");
     gets(dPaciente->nome);
     while (!validaNome(dPaciente->nome))
     {
-        printf("## Credencial negada - digite novamente\n");
+        printf("## Credencial inválida - digite novamente\n");
         printf("Nome: ");
         gets(dPaciente->nome);
     }
@@ -101,7 +98,7 @@ void cadastraPaciente(CarteiraVacinacao *dPaciente)
     gets(dPaciente->telefone);
     while (!validaTelefone(dPaciente->telefone))
     {
-        printf("## Credencial negada - digite novamente\n");
+        printf("## Credencial inválida - digite novamente\n");
         printf("Telefone: ");
         gets(dPaciente->telefone);
     }
@@ -111,29 +108,64 @@ void cadastraPaciente(CarteiraVacinacao *dPaciente)
           &dPaciente->data_nascimento.mes, &dPaciente->data_nascimento.ano);
     while (!validaData(dPaciente->data_nascimento))
     {
-        printf("## Credencial negada - digite novamente\n");
+        printf("## Credencial inválida - digite novamente\n");
         printf("Data de nascimento (DD/MM/AAAA): ");
         scanf("%d/%d/%d", &dPaciente->data_nascimento.dia,
               &dPaciente->data_nascimento.mes, &dPaciente->data_nascimento.ano);
     }
 
     printf("Quantidade de doses tomadas: ");
-    scanf("%d", &qtd_doses);
-    while (qtd_doses > MAX_DOSES || qtd_doses < 0)
+    scanf("%d", &qtdDosesPaciente);
+    while (qtdDosesPaciente > MAX_DOSES || qtdDosesPaciente < 0)
     {
-        printf("## Credencial negada - digite novamente\n");
+        printf("## Credencial inválida - digite novamente\n");
         printf("Quantidade de doses tomadas: ");
-        scanf("%d", &qtd_doses);
+        scanf("%d", &qtdDosesPaciente);
     }
 
-    __fpurge(stdin);
-
-    for (int i = 0; i < qtd_doses; i++)
+    for (int i = 0, fabricanteVacina = 0; i < qtdDosesPaciente; i++)
     {
         printf("--- Dados %dª dose ---\n", i + 1);
 
-        printf("Fabricante da vacina: ");
-        gets(dPaciente->vacina[i].fabricante);
+        printf("Fabricante da vacina (1. pfizer, 2. janssen, 3. astrazeneca, 4. moderna, 5. sinopharm, 6. outro): ");
+        while (fabricanteVacina == 0)
+        {
+            __fpurge(stdin);
+
+            scanf("%d", &fabricanteVacina);
+
+            __fpurge(stdin);
+
+            switch (fabricanteVacina)
+            {
+            case 1:
+                strcpy(dPaciente->vacina[i].fabricante, "pfizer");
+                break;
+            case 2:
+                strcpy(dPaciente->vacina[i].fabricante, "janssen");
+                break;
+            case 3:
+                strcpy(dPaciente->vacina[i].fabricante, "astrazeneca");
+                break;
+            case 4:
+                strcpy(dPaciente->vacina[i].fabricante, "moderna");
+                break;
+            case 5:
+                strcpy(dPaciente->vacina[i].fabricante, "sinopharm");
+                break;
+            case 6:
+                printf("Digite o nome do fabricante: ");
+                gets(dPaciente->vacina[i].fabricante);
+                break;
+            default:
+                printf("## Credencial inválida - digite novamente\n");
+                printf("Fabricante da vacina (1. pfizer, 2. janssen, 3. astrazeneca, 4. moderna, 5. sinopharm, 6. outro): ");
+                break;
+            }
+        }
+        fabricanteVacina = 0;
+
+        __fpurge(stdin);
 
         printf("Lote: ");
         gets(dPaciente->vacina[i].lote);
@@ -143,25 +175,26 @@ void cadastraPaciente(CarteiraVacinacao *dPaciente)
               &dPaciente->vacina[i].data_aplicacao.mes, &dPaciente->vacina[i].data_aplicacao.ano);
         while (!validaData(dPaciente->vacina[i].data_aplicacao))
         {
-            printf("## Credencial negada - digite novamente\n");
+            printf("## Credencial inválida - digite novamente\n");
             printf("Data da aplicação: ");
             scanf("%d/%d/%d", &dPaciente->vacina[i].data_aplicacao.dia,
                   &dPaciente->vacina[i].data_aplicacao.mes, &dPaciente->vacina[i].data_aplicacao.ano);
         }
         __fpurge(stdin);
     }
-    qtd_pacientesCadastrados++;
+    qtdPacientesCadastrados++;
 }
 
-void buscaNomePaciente(CarteiraVacinacao dPaciente[])
+//
+void buscaNomePaciente(CarteiraVacinacao *dPaciente)
 {
-    char nomePaciente[50];
+    char nomeBuscaPaciente[50];
     printf("\n--- Busca de paciente ---\nDigite um nome: ");
-    gets(nomePaciente);
+    gets(nomeBuscaPaciente);
 
     for (int i = 0; i < MAX_PACIENTES; i++)
     {
-        if (strcmp(nomePaciente, dPaciente[i].nome) == 0)
+        if (strcmp(nomeBuscaPaciente, dPaciente[i].nome) == 0)
         {
             printf("\n--- Dados do paciente ---\n");
             printf("Nome: %s\n", &dPaciente[i].nome);
@@ -169,72 +202,150 @@ void buscaNomePaciente(CarteiraVacinacao dPaciente[])
             printf("Data de nacimento: %d/%d/%d\n", dPaciente[i].data_nascimento.dia,
                    dPaciente[i].data_nascimento.mes, dPaciente[i].data_nascimento.ano);
 
-            for (int j = 0; j < MAX_PACIENTES; j++)
-            {
-                if (validaData(dPaciente[i].vacina[j].data_aplicacao))
+            if (validaData(dPaciente[i].vacina[0].data_aplicacao))
+                for (int j = 0; j < MAX_PACIENTES; j++)
                 {
-                    printf("--- %dª dose ---\n", j + 1);
-                    printf("Fabricante da vacina: %s\n", dPaciente[i].vacina[j].fabricante);
-                    printf("Lote: %s\n", dPaciente[i].vacina[j].lote);
-                    printf("Data da aplicação: %d/%d/%d\n", dPaciente[i].vacina[j].data_aplicacao.dia,
-                           dPaciente[i].vacina[j].data_aplicacao.mes, dPaciente[i].vacina[j].data_aplicacao.ano);
+                    if (validaData(dPaciente[i].vacina[j].data_aplicacao))
+                    {
+                        printf("--- %dª dose ---\n", j + 1);
+                        printf("Fabricante da vacina: %s\n", dPaciente[i].vacina[j].fabricante);
+                        printf("Lote: %s\n", dPaciente[i].vacina[j].lote);
+                        printf("Data da aplicação: %d/%d/%d\n", dPaciente[i].vacina[j].data_aplicacao.dia,
+                               dPaciente[i].vacina[j].data_aplicacao.mes, dPaciente[i].vacina[j].data_aplicacao.ano);
+                    }
                 }
-            }
-
+            else
+                printf("Não tomou nenhuma dose da vacina\n");
             break;
         }
     }
 }
 
-void listaDadosPaciente(CarteiraVacinacao dPaciente[])
+// Retorna os dados de todos os paciente em forma de uma tabela.
+void listaDadosPacientes(CarteiraVacinacao *dPaciente)
 {
     printf("\n----- Lista de pacientes -----\n");
 
-    // printa os dados cadastrais de cada paciente da base de dados passada
-    for (int i = 0; i < qtd_pacientesCadastrados; i++)
-    {
-        if (validaNome(dPaciente[i].nome))
-        {
-            printf("Nome: %s - ", &dPaciente[i].nome);
-            printf("Telefone: %s - ", &dPaciente[i].telefone);
-            printf("Data de nacimento: %d/%d/%d - ", dPaciente[i].data_nascimento.dia,
-                   dPaciente[i].data_nascimento.mes, dPaciente[i].data_nascimento.ano);
+    int dosesTomadas = 0;
 
-            // verifica as vacinas que o paciente tomou
-            if (validaData(dPaciente[i].vacina[0].data_aplicacao))
-                for (int j = 0; j < MAX_DOSES; j++)
-                {
-                    if (validaData(dPaciente[i].vacina[j].data_aplicacao))
-                    {
-                        printf("%dª dose - ", j + 1);
-                        printf("Fabricante da vacina: %s - ", dPaciente[i].vacina[j].fabricante);
-                        printf("Lote: %s - ", dPaciente[i].vacina[j].lote);
-                        printf("Data da aplicação: %d/%d/%d", dPaciente[i].vacina[j].data_aplicacao.dia,
-                               dPaciente[i].vacina[j].data_aplicacao.mes, dPaciente[i].vacina[j].data_aplicacao.ano);
-                    }
-                }
-            else
-                printf("Não tomou nenhuma dose da vacina");
-            printf("\n--------------------------\n");
+    // printa os dados cadastrais de cada paciente da base de dados passada
+    for (int i = 0; i < qtdPacientesCadastrados; i++)
+    {
+        printf("| %s |", &dPaciente[i].nome);
+        printf(" %s |", &dPaciente[i].telefone);
+        printf(" %d/%d/%d |", dPaciente[i].data_nascimento.dia,
+               dPaciente[i].data_nascimento.mes, dPaciente[i].data_nascimento.ano);
+
+        // conta quantas vacinas foram tomadas
+        for (int j = 0; j < MAX_DOSES; j++)
+        {
+            if (validaData(dPaciente[i].vacina[j].data_aplicacao))
+                dosesTomadas++;
         }
+        printf(" %d |", dosesTomadas);
+        printf("\n------------------------------\n");
     }
+}
+
+//
+void qtdDosesFabricante(CarteiraVacinacao dPaciente[])
+{
+    // cria um vetor que guarda numero de pessoas que tomaram cada uma das doses
+    // 0 = pfizer, 1 = janssen, 2 = astrazeneca, 3 = moderna, 4 = sinopharm, 5 = outro
+    int vetDosesAplicadasFab[] = {0, 0, 0, 0, 0, 0};
+
+    for (int i = 0; i < qtdPacientesCadastrados; i++)
+        for (int j = 0; j < MAX_DOSES; j++)
+            if (validaData(dPaciente[i].vacina[j].data_aplicacao))
+            {
+                // printf("%s", dPaciente[i].vacina[j].fabricante);
+                if (strcmp(dPaciente[i].vacina[j].fabricante, "pfizer") == 0)
+                    vetDosesAplicadasFab[0]++;
+                else if (strcmp(dPaciente[i].vacina[j].fabricante, "janssen") == 0)
+                    vetDosesAplicadasFab[1]++;
+                else if (strcmp(dPaciente[i].vacina[j].fabricante, "astrazeneca") == 0)
+                    vetDosesAplicadasFab[2]++;
+                else if (strcmp(dPaciente[i].vacina[j].fabricante, "moderna") == 0)
+                    vetDosesAplicadasFab[3]++;
+                else if (strcmp(dPaciente[i].vacina[j].fabricante, "sinopharm") == 0)
+                    vetDosesAplicadasFab[4]++;
+                else
+                    vetDosesAplicadasFab[5]++;
+            }
+
+    printf("\n--- Quantidade de dose aplicadas de cada uma das fabricantes ---\n");
+    printf("Pfizer: %d\n", vetDosesAplicadasFab[0]);
+    printf("Janssen: %d\n", vetDosesAplicadasFab[1]);
+    printf("Astrazeneca: %d\n", vetDosesAplicadasFab[2]);
+    printf("Moderna: %d\n", vetDosesAplicadasFab[3]);
+    printf("Sinopharm: %d\n", vetDosesAplicadasFab[4]);
+    printf("Outras: %d\n", vetDosesAplicadasFab[5]);
+}
+
+//
+void qtdDosesAplicadas(CarteiraVacinacao dPaciente[])
+{
+    // cria um vetor que guarda numero de pessoas que tomaram cada uma das doses
+    int vetDosesAplicadas[MAX_DOSES] = {0};
+
+    for (int i = 0; i < qtdPacientesCadastrados; i++)
+        for (int j = 0; j < MAX_DOSES; j++)
+            if (validaData(dPaciente[i].vacina[j].data_aplicacao))
+                vetDosesAplicadas[j]++;
+    printf("\n--- Quantidade de pacientes que receberam cada uma das doses ---\n");
+    for (int i = 0; i < MAX_DOSES; i++)
+        printf("%dª dose: %d - Porcentagem: %d %%\n", i + 1, vetDosesAplicadas[i], vetDosesAplicadas[i] / MAX_PACIENTES * 100);
+}
+
+void inicializaSistema()
+{
+
 }
 
 int main()
 {
+    system("clear");
     CarteiraVacinacao paciente[MAX_PACIENTES];
-
-    int qtd_pacientes;
-    printf("Digite quantos pacientes deseja cadastrar: ");
-
-    scanf("%d", &qtd_pacientes);
-    __fpurge(stdin);
-    for (int i = 0; i < qtd_pacientes; i++)
+    int opcao;
+    do
     {
-        printf("\n--- Paciente %d ---\n", i + 1);
+        printf("\n=========== SISTEMA COVID ==========================\n");
+        printf("(1) Cadastrar um paciente\n");
+        printf("(2) Buscar paciente pelo nome\n");
+        printf("(3) Listar pacientes cadastrados\n");
+        printf("(4) Quantidade doses aplicadas de cada fabricante\n");
+        printf("(5) Quantidade de pacientes vacinados por cada dose\n");
+        printf("(6) Sair do sistema\n");
 
-        cadastraPaciente(&paciente[i]);
-    }
+        printf("============== MENU ================================\n");
+        printf("Digite uma opção: ");
 
-    listaDadosPaciente(&paciente[0]);
+        scanf("%d", &opcao);
+        __fpurge(stdin);
+
+        switch (opcao)
+        {
+        case 1:
+            cadastraPaciente(&paciente[qtdPacientesCadastrados]);
+            break;
+        case 2:
+            buscaNomePaciente(&paciente);
+            break;
+        case 3:
+            listaDadosPacientes(&paciente);
+            break;
+        case 4:
+            qtdDosesFabricante(&paciente);
+            break;
+        case 5:
+            qtdDosesAplicadas(&paciente);
+            break;
+        case 6:
+            printf("Saindo do sistema...\n");
+            break;
+        default:
+            printf("## Opção inválida\n");
+            break;
+        }
+    } while (opcao != 6);
 }
